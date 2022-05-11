f6d92088f5674a52b9f1c6ef45acfd800cf1f8ed
Jason-TianrenTan <13522330013@163.com>
2022-05-11 11:43:45 -0700

    add username and password validation

--- bank/static/confirm.js
+++ bank/static/confirm.js
@@ -1,9 +1,30 @@
+var regex = new RegExp("^[a-z0-9_.-]*$");
+
 function onChange() {
-  const password = document.querySelector('input[id=password]');
-  const confirm = document.querySelector('input[id=confirm]');
-  if (confirm.value === password.value) {
-    confirm.setCustomValidity('');
-  } else {
-    confirm.setCustomValidity('Passwords do not match');
-  }
+    const password = document.querySelector('input[id=password]');
+    const confirm = document.querySelector('input[id=confirm]');
+    if (testPassword()) {
+        password.setCustomValidity('');
+        if (confirm.value === password.value) {
+            confirm.setCustomValidity('');
+        } else {
+            confirm.setCustomValidity('Passwords do not match');
+        }
+    } else {
+        password.setCustomValidity('Account passwords are restricted to lowercase letters, numbers, dots, hyphens and underscores')
+    }
+}
+
+function verifyUserName() {
+    const username = document.querySelector('input[id=username]')
+    if (regex.test(username.value)) {
+        username.setCustomValidity('');
+    } else {
+        username.setCustomValidity('Account names are restricted to lowercase letters, numbers, dots, hyphens and underscores');
+    }
+}
+
+function testPassword() {
+    const password = document.querySelector('input[id=password]')
+    return regex.test(password.value);
 }
