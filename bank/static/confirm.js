43b561a3fd43585bb31a96c00d9143efe2a6c2e7
Jason-TianrenTan <13522330013@163.com>
2022-05-11 12:29:26 -0700

    refactor js

--- bank/static/confirm.js
+++ bank/static/confirm.js
@@ -1,35 +1,42 @@
 var regex = new RegExp("^[a-z0-9_.-]*$");
 
-function onChange() {
-    const password = document.querySelector('input[id=password]');
-    const confirm = document.querySelector('input[id=confirm]');
-    if (testPassword()) {
-        password.setCustomValidity('');
-        if (confirm.value === password.value) {
-            confirm.setCustomValidity('');
+function passOnChange() {
+    const password = $("#password").val(),
+        confirm = $("#confirm").val();
+    if (regex.test(password)) {
+        $("#password").get(0).setCustomValidity('');
+        if (confirm === password) {
+            $("#confirm").get(0).setCustomValidity('');
         } else {
-            confirm.setCustomValidity('Passwords do not match');
+            $("#confirm").get(0).setCustomValidity('Passwords do not match');
         }
     } else {
-        password.setCustomValidity('Account passwords are restricted to lowercase letters, numbers, dots, hyphens and underscores')
+        $("#password").get(0).setCustomValidity('Account passwords are restricted to lowercase letters, numbers, dots, hyphens and underscores')
     }
 }
 
-function verifyUserName() {
-    const username = document.querySelector('input[id=username]')
-    if (regex.test(username.value)) {
-        username.setCustomValidity('');
-    } else {
-        username.setCustomValidity('Account names are restricted to lowercase letters, numbers, dots, hyphens and underscores');
-    }
-}
+$(document).ready(function () {
+    $("#username").change(function () {
+        const username = $("#username").val();
+        if (regex.test(username)) {
+            $("#username").get(0).setCustomValidity('');
+        } else {
+            $("#username").get(0).setCustomValidity('Account names are restricted to lowercase letters, numbers, dots, hyphens and underscores');
+        }
+    });
 
-function testPassword() {
-    const password = document.querySelector('input[id=password]')
-    return regex.test(password.value);
-}
+    $("#balance").change(function () {
+        const balance = $("#balance").val();
+        $("#balance").val(parseFloat(balance).toFixed(2));
+    })
 
-function setTwoNumberDecimal() {
-    const balance = document.querySelector('input[id=balance]')
-    balance.value = parseFloat(balance.value).toFixed(2);
-}
+    $("#password").change(function () {
+        passOnChange();
+    });
+
+    $("#confirm").change(function () {
+        passOnChange();
+    });
+})
+
+
