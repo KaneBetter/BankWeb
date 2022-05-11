a97ac09a94b04f6319757e9a0d9340c67ebc5de8
Zejin <zejin101@gmail.com>
2022-05-12 03:13:42 +0800

    Merge floating support to main (#4)
    * Add balance regex validation
    
    * Add constraints on initial amount

--- bank/index.py
+++ bank/index.py
@@ -3,10 +3,8 @@ import logging
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from flask_wtf import FlaskForm as Form
-from wtforms import IntegerField
-from wtforms.validators import DataRequired
-
-from bank import models
+from wtforms import StringField
+from wtforms.validators import DataRequired, Regexp
 
 index_bp = Blueprint('index', __name__)
 logger = logging.getLogger(__name__)
@@ -24,4 +22,5 @@ def index():
 
 
 class TransactionForm(Form):
-    amount = IntegerField(validators=[DataRequired()])
+    amount = StringField(validators=[DataRequired(),
+        Regexp("^(0|[1-9][0-9]*){1}(\.[0-9]{2})?$", message="Invalid input.")])
