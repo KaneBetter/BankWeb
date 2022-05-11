7e3c7142b7247cccecefa03cf2f080276e94a00a
rustberry <zejin101@hotmail.com>
2022-05-10 18:13:17 -0700

    Add support for FlaskForm validation, especially Email format validation

--- bank/index.py
+++ bank/index.py
@@ -4,6 +4,7 @@ from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from flask_wtf import FlaskForm as Form
 from wtforms import IntegerField
+from wtforms.validators import DataRequired
 
 from bank import models
 
@@ -24,4 +25,4 @@ def index():
 
 
 class TransactionForm(Form):
-    amount = IntegerField()
+    amount = IntegerField(validators=[DataRequired()])
